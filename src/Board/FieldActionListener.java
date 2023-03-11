e046bf3d42197e083bb5155b0b28f7a194a2ec1c
winzig3r <75442801+winzig3r@users.noreply.github.com>
2023-03-11 15:36:50 +0100

    Finished now

--- src/Board/FieldActionListener.java
+++ src/Board/FieldActionListener.java
@@ -1,31 +1,66 @@
 package Board;
 
-import javax.swing.*;
-import java.awt.*;
+import GameManaging.CastleMove;
+import GameManaging.GameManager;
+import Pieces.King;
+import Pieces.Piece;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class FieldActionListener implements ActionListener {
 
-    final JButton self;
-    final Color origColor;
-    final Color selectedColor;
-
-    private int clickCount = 0;
-    public FieldActionListener(JButton self){
+    final Field self;
+    public FieldActionListener(Field self){
         this.self = self;
-        this.origColor = self.getBackground();
-        this.selectedColor = new Color(218, 196, 49);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(clickCount % 2 == 0){
-            self.setBackground(this.selectedColor);
-        }else {
-            self.setBackground(this.origColor);
+        if (self.isMovable()) {
+            Field startField = Board.getSelectedField();
+            movePieceToSelf(startField);
+            checkEnPassant();
+            checkCastles();
+            Board.getPiecePanel().repaint();
+            GameManager.progressTurn();
         }
+        if(self.getPieceOnField() == null && Board.getSelectedField() != null){
+            Board.getSelectedField().setSelected(false);
+        }
+        if(self.getPieceOnField() != null && GameManager.getCurrentTurn() == self.getPieceOnField().getTeam()){
+            if(self.getPieceOnField() != null && Board.getSelectedField() != null && !self.isSelected()){
+                Board.getSelectedField().setSelected(false);
+                self.setSelected(true);
+            } else if(self.getPieceOnField() != null){
+                self.setSelected(!self.isSelected());
+            }
+        }
+    }
 
-        clickCount++;
+    public void movePieceToSelf(Field startField){
+        Piece pieceToMove = Board.getSelectedField().getPieceOnField();
+        //If a piece was captured
+        self.removePieceFromSelf();
+
+        startField.setSelected(false);
+        pieceToMove.moveToNewField(self);
+        pieceToMove.moved();
+    }
+
+    private void checkEnPassant(){
+        Field enPassant = GameManager.getEnPassant(self);
+        if(enPassant != null){
+            enPassant.removePieceFromSelf();
+            enPassant.setPieceOnField(null);
+        }
+    }
+
+    private void checkCastles(){
+        CastleMove castleMove = GameManager.getCastle(self);
+        if(castleMove != null){
+            castleMove.rookToCastle.moveToNewField(castleMove.newRookField);
+            GameManager.clearCastleMoves();
+        }
     }
 }
